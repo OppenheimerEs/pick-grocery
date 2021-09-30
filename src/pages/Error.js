5aa11162fec4708cc25c5f4ca0777775d263bcef
wormx64 <77200870+OppenheimerEs@users.noreply.github.com>
2021-09-30 20:17:40 +0100

    Update Error.js

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,18 +1,18 @@
-import {useLocation} from 'react-router-dom'
-import {Helmet} from 'react-helmet'
+// import {useLocation} from 'react-router-dom'
+// import {Helmet} from 'react-helmet'
 
 function Error() {
-    const location = useLocation()
+//     const location = useLocation()
     return (
         <>
-        <Helmet>
-            <title>404 Page</title>
-        </Helmet>
+//         <Helmet>
+//             <title>404 Page</title>
+//         </Helmet>
         <section className='error-page'>
-            <div>
-                <h1>404</h1>
-                <h4>No Such Path Name : <code><pre>{location.pathname}</pre></code></h4>
-            </div>
+//             <div>
+//                 <h1>404</h1>
+//                 <h4>No Such Path Name : <code><pre>{location.pathname}</pre></code></h4>
+//             </div>
         </section>
         </>
     )
